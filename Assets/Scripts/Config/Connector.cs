33f14e1e50cf526e51f31d09a6af8c098b45661f
Nam Pham <hainam.pham@innogames.com>
2019-06-22 03:05:47 +0200

    cycle through different connector

--- Assets/Scripts/Config/Connector.cs
+++ Assets/Scripts/Config/Connector.cs
@@ -5,6 +5,7 @@ namespace Config
 	public class Connector : MonoBehaviour
 	{
 		public Connector ConnectedTo;
+		public int ConnectedToIndex = 0;
 		public ConnectorType ConnectorType;
 		public Node[] SharedNodes;
 	}
--- Assets/Scripts/Config/Editor/RoadSegmentEditor.cs
+++ Assets/Scripts/Config/Editor/RoadSegmentEditor.cs
@@ -8,17 +8,20 @@ namespace Config
 	public class RoadSegmentEditor: Editor
 	{
 		private Connector _selectedConnector;
+		private int _selectedIndex = -1;
 		private void OnSceneGUI()
 		{
-			RoadSegment segment = (RoadSegment)target;
-
-			foreach (var connector in segment.Connectors)
+			var segment = (RoadSegment)target;
+			
+			for (int i = 0; i < segment.Connectors.Length; i++)
 			{
+				var connector = segment.Connectors[i];
 				Handles.color = connector == _selectedConnector ? Color.green : Color.white;
 				if (Handles.Button(connector.transform.position, connector.transform.rotation, 3f, 3f, Handles.ConeHandleCap))
 				{
 					_selectedConnector = connector;
-				}				
+					_selectedIndex = i;
+				}
 			}
 		}
 
@@ -52,15 +55,27 @@ namespace Config
 					var texture = AssetPreview.GetAssetPreview(other.gameObject);
 					if (GUILayout.Button(texture, GUILayout.Width(IconSize), GUILayout.Height(IconSize)))
 					{
-						int index = -1;
-						for (index = 0; index < other.Connectors.Length; index++)
+						int index = 0;
+						if (_selectedConnector.ConnectedTo != null)
+						{
+							var exist = _selectedConnector.ConnectedTo.GetComponentInParent<RoadSegment>();
+							if (PrefabUtility.GetCorrespondingObjectFromOriginalSource(exist) == other)
+							{
+								index = (_selectedConnector.ConnectedToIndex + 1) % other.Connectors.Length;								
+							}
+							DestroyImmediate(exist.gameObject);
+							_selectedConnector.ConnectedTo = null;
+							_selectedConnector.ConnectedToIndex = 0;
+						}
+
+						for (; index < other.Connectors.Length; index++)
 						{
 							if (_selectedConnector.ConnectorType == other.Connectors[index].ConnectorType)
 							{
 								break;
 							}
 						}
-						PlaceSegment(segment.transform, _selectedConnector, other, index);
+						PlaceSegment(segment.transform, _selectedConnector, other, index);							
 					}
 				}
 
@@ -87,7 +102,9 @@ namespace Config
 			newSegment.transform.position = position - newSegment.transform.rotation * otherConnector.transform.localPosition;
 
 			myConnector.ConnectedTo = otherConnector;
+			myConnector.ConnectedToIndex = otherConnectorId;
 			otherConnector.ConnectedTo = myConnector;
+			otherConnector.ConnectedToIndex = _selectedIndex;
 			
 			Undo.RecordObject(newSegment.gameObject, "Newly connect Segment");
 		}
