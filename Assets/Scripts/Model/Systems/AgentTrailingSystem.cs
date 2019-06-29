969410102a567d22998c502f9b1064105fa4b2a8
Nam Pham <hainam.pham@innogames.com>
2019-06-29 13:38:56 +0200

    first version of agent trailing using int

--- Assets/Scripts/Model/Systems/AgentTrailingSystem.cs
+++ Assets/Scripts/Model/Systems/AgentTrailingSystem.cs
@@ -14,85 +14,51 @@ namespace Model.Systems
 	{
 		protected override JobHandle OnUpdate(JobHandle inputDeps)
 		{
-			//propose: race condition!
-			//enter connection:
-			//- only agent at the end of con
-			//=> both curCon and nextCon are unique. nextCon could collide with curCon
-			//- input: nextCon.state
-			//- input: nextCon.traffic
-			//- input: nextCon.trail
-			//- input: curCon.state
-			//- in/out: agent.coord, agent.state
-			//- output: nextCon.trail
-			//- output: nextCon.state
-			//- output: curCon.state, only if curCon has 1 agent --> will CLASH with nextCon
-			//- scenario: X enter BC, Y is leaving BC
-			//- X output to BC.EnterLen
-			//- Y output to BC.EnterLen => clash!
-			//enter connection.chill: for agent with moveDist == 0 & not at end of con: do nothing!
-			//pull
-			//- only agent with moveDist > 0 & not end of con & agent.state == pull
-			//- input: agent.trail
-			//- in/out: agent.coord, agent.state
-			//- output: trailAgent.state -> no clash with agent.state?
-			//- - apply to only trailAgent.state != pull => good!
-			//- - set trailAgent.state to pull => bad!
-			//- because if an agent state is Pull, it just has a new coord (from pulled, or end of con) => no need to pull it!
-			//move forward
-			//- only agent with moveDist > 0 & not end of con
-			//- in/out: agent.coord, agent.trail, agent.state
-			//- no pointer output!
-			
-			//propose: race condition!
-			//enter connection: per agent: at end con
-			//- output: nextCon.EnterSpace: occupy space
-			//- input: nextCon.trailAgent
-			//- output: nextCon.trailAgent.trailAgent = agent
-			//- output: nextCon.trailAgent
-			//- output: agent.trailAgent.moveDist
-			//- output: agent.moveDist
-			//- output: agent.trailAgent: CLASH with nextCon.trailAgent
-			//pull: per agent: with headCon.moveDist > 0
-			//- agent.moveDist+
-			//- output: tailCon.moveDist => got clean up!
-			//clear: per con with con.moveDist > 0
-			//- con.EnterLen += moveDist
-			//- con.moveDist = 0
-			
 			//propose 2: con.pullCord could have more than one value? NO! there is ONLY one gap at a time!
+			//
 			//first agent move: if agent.moveDist == 0 (prune) && agent.headCord == headCon.len (imply agent.moveDist == 0)
-			//- in/out: nextCon.EnterLen
-			//- in/out: agent.headCon, moveDist
-			//- output: tailCon.pullCord, hasPull
+			//agent input:  headCord, tailCon, tailCord 
+			//agent in/out: headCon, headCord
+			//agent output: moveDist
+			//con input:    headCon.len
+			//con in/out:   nextCon.EnterLen
+			//con output:   tailCon.pullCord, pullDist
+			//
 			//- agent.moveDist = nextCon.EnterLen
+			//- agent.headCon = nextCon
+			//- agent.headCord = 0
 			//- tailCon.pullCord = agent.tailCord //pullCord CREATE
 			//- tailCon.pullDist = moveDist
-			//- agent.headCon = nextCon
-			//- nextCon.EnterLen -= min(agent.len, nextCon.EnterLen) //EnterLen DECREASE!
-			
+			//- nextCon.EnterLen -= min(agent.len, moveDist) //EnterLen DECREASE!
+
 			//pull: if agent.moveDist == 0 & headCon.pullDist > 0 & agent.headCord == headCon.pullCord(max one agent per con)
-			//- input: headCon.len, headCon.pullCord
-			//- input: agent.headCord
-			//- in/out: agent.moveDist
-			//- output: tailCon.pullCord
+			//agent input:  headCon, headCord, tailCon, tailCord, moveDist
+			//agent output: moveDist
+			//con input:    len, pullCord, pullDist
+			//con output:   pullCord, pullDist
+			//
 			//- pull = min(headCon.pullDist, headCon.len - agent.headCord)
 			//- agent.moveDist += pull
-			//- if (tailCon != headCon) //pullCord CREATE
-			//- - tailCon.pullCord = agent.tailCord
-			//- - tailCon.pullDist = pull
-			//- - headCon.pullCord = headCon.EnterLen //which is 0 ==> to clear
-			//- else
-			//- - headCon.pullCord = agent.tailCord
-			
+			//- tailCon.pullCord = agent.tailCord //pullCord CREATE
+			//- tailCon.pullDist = pull
+
 			//last agent move: one match per connection! no clash!
+			//agent input: tailCon, tailCord, moveDist
+			//con input:   EnterLen, pullCord, pullDist, len
+			//con output:  EnterLen, pullCord, pullDist
+			//
 			//- if (agent.tailCord == tailCon.EnterLen) //last agent
 			//- - move = min(tailCon.len - agent.tailCord, agent.moveDist)
 			//- - tailCon.EnterLen += move //EnterLen INCREASE!
 			//- - tailCon.pullCord += move //PullCord INCREASE! (until conLen)
 			//- - tailCon.pullDist -= move
 
-			//move: agent.moveDist > 0
-			//- in/out: agent.headCord, tailCord, moveDist
+			//move forward: agent.moveDist > 0
+			//agent input:  headCon, headCord, tailCon, tailCord, moveDist
+			//agent output: headCord, tailCon, tailCord, moveDist
+			//con input:    speed, len
+			//con output:   none
+			//
 			//- speed = agent.headCon.speed
 			//- move = min (speed, agent.moveDist) //moveDist is already not exceed con.len!
 			//- move = min (move, tailCon.len - agent.tailCord) //lose speed artificially :( //could loop here for tailCord!
@@ -100,24 +66,55 @@ namespace Model.Systems
 			//- agent.tailCord += move
 			//- agent.moveDist -= move
 			//- if (agent.tailCord == tailCon.len) tailCon = next(tailCon)
+
+			var conLens = GetComponentDataFromEntity<ConnectionLengthInt>();
+			var connections = GetComponentDataFromEntity<Connection>();
+			var conTraffics = GetComponentDataFromEntity<ConnectionTraffic>();
+			var conSpeeds = GetComponentDataFromEntity<ConnectionSpeedInt>();
+			var indexes = GetComponentDataFromEntity<IndexInNetwork>();
+			var states = GetComponentDataFromEntity<ConnectionStateInt>();
+			var pulls = GetComponentDataFromEntity<ConnectionPullInt>();
+			var next = GetBufferFromEntity<NextBuffer>();
 			
-			var moveJob = new EnterConnection
+			var firstAgentMove = new FirstAgentMove
 			{
-				AgentStates = GetComponentDataFromEntity<AgentStateInt>(),
-				ConLens = GetComponentDataFromEntity<ConnectionLengthInt>(),
-				Connections = GetComponentDataFromEntity<Connection>(),
-				ConTraffics = GetComponentDataFromEntity<ConnectionTraffic>(),
-				Indexes = GetComponentDataFromEntity<IndexInNetwork>(),
-				States = GetComponentDataFromEntity<ConnectionStateInt>(),
-				TrailAgents = GetComponentDataFromEntity<TrailAgent>(),
-				Next = GetBufferFromEntity<NextBuffer>(),
+				Connections = connections,
+				ConLens = conLens,
+				ConTraffics = conTraffics,
+				Indexes = indexes,
+				Next = next,
+				States = states,
+				Pulls = pulls,
 			}.Schedule(this, inputDeps);
 
-			return moveJob;
+			var pullPropagate = new PullPropagate
+			{
+				ConLens = conLens,
+				Pulls = pulls,
+			}.Schedule(this, firstAgentMove);
+
+			var lastAgentMove = new LastAgentMove
+			{
+				ConLens = conLens,
+				Pulls = pulls,
+				States = states,
+			}.Schedule(this, pullPropagate);
+
+			var moveForward = new MoveForward
+			{
+				Connections = connections,
+				ConLens = conLens,
+				Indexes = indexes,
+				Next = next,
+				ConSpeeds = conSpeeds,
+				Pulls = pulls,
+			}.Schedule(this, lastAgentMove);
+			
+			return moveForward;
 		}
 
 		[BurstCompile]
-		private struct EnterConnection : IJobForEachWithEntity<AgentInt, ConnectionTarget, AgentCoordInt, AgentStateInt>
+		private struct FirstAgentMove : IJobForEachWithEntity<AgentInt, ConnectionTarget, AgentCordInt, AgentStateInt>
 		{
 			[ReadOnly] public ComponentDataFromEntity<Connection> Connections;
 			[ReadOnly] public ComponentDataFromEntity<ConnectionLengthInt> ConLens;
@@ -126,57 +123,49 @@ namespace Model.Systems
 			[ReadOnly] public BufferFromEntity<NextBuffer> Next;
 
 			[NativeDisableParallelForRestriction] public ComponentDataFromEntity<ConnectionStateInt> States;
-			[NativeDisableParallelForRestriction] public ComponentDataFromEntity<TrailAgent> TrailAgents;
+			[NativeDisableParallelForRestriction] public ComponentDataFromEntity<ConnectionPullInt> Pulls;
 
 			public void Execute(Entity agentEnt, int index,
 				[ReadOnly] ref AgentInt agent,
 				[ReadOnly] ref ConnectionTarget target,
-				ref AgentCoordInt coord,
+				ref AgentCordInt cord,
 				ref AgentStateInt agentState)
 			{
-				var curConEnt = coord.Connection;
-				int curConLen = ConLens[curConEnt].Length;
-				bool endOfCon = coord.CurCoord == curConLen;
-				//because moveDist may overshoot conLen and remain positive at end con
-				if (agentState.MoveDist == 0 || endOfCon)
+				if (agentState.MoveDist == 0)
 				{
+					var headConEnt = cord.HeadCon;
+					int headConLen = ConLens[headConEnt].Length;
+					bool endHeadCon = cord.HeadCord == headConLen;
+					//because moveDist may overshoot conLen and remain positive at end con
+
 					//reached target
-					if (endOfCon) //end of connection
+					if (endHeadCon) //end of connection
 					{
+						//TODO check reach final target!
 						//compute next path
-						var nextConEnt = ComputeNextCon(ref target, ref curConEnt);
+						var nextConEnt = ComputeNextCon(ref target, ref headConEnt);
 
 						var nextTraffic = ConTraffics[nextConEnt];
 						var nextState = States[nextConEnt];
 						if (nextTraffic.TrafficType != ConnectionTrafficType.NoEntrance &&
 						    nextState.EnterLength > 0)
 						{
-							//update next connection
 							int moveDist = nextState.EnterLength;
-							agentState.MoveDist = moveDist; //also discard excess modeDist from last con!
-							agentState.State = AgentState.Pull;
-							
-							//TODO perform pulling here, because the pulled agent can't be first in any connection!
-							
-							nextState.EnterLength = math.max(0, nextState.EnterLength - agent.Length);
+							//update agent
+							agentState.MoveDist = moveDist;
+							cord.HeadCon = nextConEnt;
+							cord.HeadCord = 0;
 
-							//update trail agent
-							var nextStateTrailAgentEnt = nextState.TrailAgent;
-							if (nextStateTrailAgentEnt != Entity.Null)
+							//create pull
+							Pulls[agentState.TailCon] = new ConnectionPullInt
 							{
-								var trailAgent = TrailAgents[nextStateTrailAgentEnt];
-								trailAgent.Agent = agentEnt;
-								TrailAgents[nextStateTrailAgentEnt] = trailAgent; //no clash here
-							}
+								PullCord = agentState.TailCord,
+								PullDist = moveDist,
+							};
 
-							nextState.TrailAgent = agentEnt;
-
+							//update next connection
+							nextState.EnterLength = math.max(0, nextState.EnterLength - agent.Length);
 							States[nextConEnt] = nextState; //may clash here!
-
-							//update cur connection
-							var state = States[curConEnt];
-							state.EnterLength += math.min(moveDist, agent.Length);
-							States[curConEnt] = state; //no clash
 						}
 						else
 						{
@@ -204,73 +193,109 @@ namespace Model.Systems
 		}
 
 		[BurstCompile]
-		private struct MovingForward : IJobForEach<AgentInt, ConnectionTarget, AgentCoordInt, TrailAgent,
-			AgentStateInt>
+		private struct PullPropagate : IJobForEachWithEntity<AgentInt, AgentCordInt, AgentStateInt>
 		{
-			[ReadOnly] public ComponentDataFromEntity<Connection> Connections;
 			[ReadOnly] public ComponentDataFromEntity<ConnectionLengthInt> ConLens;
-			[ReadOnly] public ComponentDataFromEntity<IndexInNetwork> Indexes;
-			[ReadOnly] public BufferFromEntity<NextBuffer> Next;
 
-			private Entity ComputeNextCon(ref ConnectionTarget target, ref Entity curConEnt)
-			{
-				var startNode = Connections[curConEnt].EndNode;
-				var targetConnectionEnt = target.Connection;
-				var endNode = Connections[targetConnectionEnt].StartNode;
-				var nextConEnt = startNode == endNode
-					? targetConnectionEnt
-					: Next[startNode][Indexes[endNode].Index].Connection;
-				return nextConEnt;
-			}
+			[NativeDisableParallelForRestriction] public ComponentDataFromEntity<ConnectionPullInt> Pulls;
 
-			public void Execute([ReadOnly] ref AgentInt agent,
-				[ReadOnly] ref ConnectionTarget target,
-				ref AgentCoordInt coord,
-				ref TrailAgent trailAgent,
+			public void Execute(Entity agentEnt, int index,
+				[ReadOnly] ref AgentInt agent,
+				[ReadOnly] ref AgentCordInt cord,
 				ref AgentStateInt agentState)
 			{
-				var curConEnt = coord.Connection;
-				int curConLen = ConLens[curConEnt].Length;
-				bool endOfCon = coord.CurCoord == curConLen;
-				//because moveDist may overshoot conLen and remain positive at end con
-				if (agentState.MoveDist > 0 && !endOfCon)
+				if (agentState.MoveDist == 0)
 				{
-					//move forward
-					int moveDist = math.min(agentState.MoveDist, agent.Speed);
-					agentState.MoveDist -= moveDist;
-					coord.CurCoord += moveDist;
-
-					//update tail
-					var tailConEnt = coord.TailCon;
-					int tailConLen = ConLens[tailConEnt].Length;
-					int newTail = coord.CurTail + moveDist - tailConLen; //in case it cross the conLen
-					if (newTail >= 0)
+					var headConEnt = cord.HeadCon;
+					var headConPull = Pulls[headConEnt];
+					if (headConPull.PullDist > 0 && cord.HeadCord == headConPull.PullCord)
 					{
-						var nextTailCon = ComputeNextCon(ref target, ref tailConEnt);
-						coord.TailCon = nextTailCon;
-
-						trailAgent.Agent = Entity.Null; //tail get to new connection, now has no trail!
-						coord.CurTail = newTail;
-					}
-					else
-					{
-						coord.CurTail += moveDist;
+						int pullDist = math.min(headConPull.PullDist, ConLens[headConEnt].Length - cord.HeadCord);
+						agentState.MoveDist += pullDist;
+						Pulls[agentState.TailCon] = new ConnectionPullInt
+						{
+							PullCord = agentState.TailCord,
+							PullDist = pullDist,
+						};
 					}
 				}
 			}
 		}
 
 		[BurstCompile]
-		private struct Pulling : IJobForEachWithEntity<AgentInt, ConnectionTarget, AgentCoordInt, AgentStateInt>
+		private struct LastAgentMove : IJobForEachWithEntity<AgentInt, AgentStateInt>
+		{
+			[ReadOnly] public ComponentDataFromEntity<ConnectionLengthInt> ConLens;
+
+			[NativeDisableParallelForRestriction] public ComponentDataFromEntity<ConnectionPullInt> Pulls;
+			[NativeDisableParallelForRestriction] public ComponentDataFromEntity<ConnectionStateInt> States;
+
+			public void Execute(Entity agentEnt, int index,
+				[ReadOnly] ref AgentInt agent,
+				[ReadOnly] ref AgentStateInt agentState)
+			{
+				var tailConEnt = agentState.TailCon;
+				var tailConState = States[tailConEnt];
+				if (agentState.TailCord == tailConState.EnterLength) //last agent
+				{
+					int moveDist = math.min(ConLens[tailConEnt].Length - agentState.TailCord, agentState.MoveDist);
+					tailConState.EnterLength += moveDist;
+					States[tailConEnt] = tailConState;
+
+					var tailPull = Pulls[tailConEnt];
+					tailPull.PullCord += moveDist;
+					tailPull.PullDist -= moveDist;
+					Pulls[tailConEnt] = tailPull;
+				}
+			}
+		}
+
+		[BurstCompile]
+		private struct MoveForward : IJobForEachWithEntity<AgentInt, ConnectionTarget, AgentCordInt, AgentStateInt>
 		{
 			[ReadOnly] public ComponentDataFromEntity<Connection> Connections;
 			[ReadOnly] public ComponentDataFromEntity<ConnectionLengthInt> ConLens;
-			[ReadOnly] public ComponentDataFromEntity<ConnectionTraffic> ConTraffics;
 			[ReadOnly] public ComponentDataFromEntity<IndexInNetwork> Indexes;
 			[ReadOnly] public BufferFromEntity<NextBuffer> Next;
+			
+			[ReadOnly] public ComponentDataFromEntity<ConnectionSpeedInt> ConSpeeds;
 
-			[NativeDisableParallelForRestriction] public ComponentDataFromEntity<TrailAgent> TrailAgents;
-			[NativeDisableParallelForRestriction] public ComponentDataFromEntity<AgentStateInt> AgentStates;
+//			[NativeDisableParallelForRestriction] public ComponentDataFromEntity<ConnectionStateInt> States;
+			[NativeDisableParallelForRestriction] public ComponentDataFromEntity<ConnectionPullInt> Pulls;
+
+			public void Execute(Entity agentEnt, int index,
+				[ReadOnly] ref AgentInt agent,
+				[ReadOnly] ref ConnectionTarget target,
+				ref AgentCordInt cord,
+				ref AgentStateInt agentState)
+			{
+				if (agentState.MoveDist > 0)
+				{
+					int speed = ConSpeeds[cord.HeadCon].Speed;
+					int moveDist = math.min(speed, agentState.MoveDist);
+					
+					cord.HeadCord += moveDist;
+
+					while (moveDist > 0)
+					{
+						var tailConEnt = agentState.TailCon;
+						int tailConLen = ConLens[tailConEnt].Length;
+						int curDist = math.min(moveDist, tailConLen - agentState.TailCord);
+
+						agentState.TailCord += curDist;
+						agentState.MoveDist -= curDist;
+
+						if (agentState.TailCord == tailConLen)
+						{
+							agentState.TailCon = ComputeNextCon(ref target, ref tailConEnt);
+							agentState.TailCord = 0;
+						}
+
+						moveDist = 0; // use the one below, but have to clear EnterLen and Pull
+//						moveDist -= curDist;
+					}
+				}
+			}
 
 			private Entity ComputeNextCon(ref ConnectionTarget target, ref Entity curConEnt)
 			{
@@ -281,35 +306,6 @@ namespace Model.Systems
 					? targetConnectionEnt
 					: Next[startNode][Indexes[endNode].Index].Connection;
 				return nextConEnt;
-			}
-
-			public void Execute(Entity agentEnt, int index,
-				[ReadOnly] ref AgentInt agent,
-				[ReadOnly] ref ConnectionTarget target,
-				ref AgentCoordInt coord,
-				ref AgentStateInt agentState)
-			{
-				var curConEnt = coord.Connection;
-				int curConLen = ConLens[curConEnt].Length;
-				bool endOfCon = coord.CurCoord == curConLen;
-				//because moveDist may overshoot conLen and remain positive at end con
-				if (agentState.MoveDist > 0 && !endOfCon)
-				{
-					//pull trail
-					if (agentState.State == AgentState.Pull) //can pull
-					{
-						var trailAgent = TrailAgents[agentEnt];
-						if (trailAgent.Agent != Entity.Null)
-						{
-							var trailAgentState = AgentStates[trailAgent.Agent];
-							trailAgentState.MoveDist += agentState.MoveDist; //NOTE this may overshoot the tailConLen!
-							trailAgentState.State = AgentState.Pull; //propagate the pull
-							AgentStates[trailAgent.Agent] = trailAgentState; //CLASH with agentState assignment!
-						}
-
-						agentState.State = AgentState.Move; //can no longer pull
-					}
-				}
 			}
 		}
 	}
