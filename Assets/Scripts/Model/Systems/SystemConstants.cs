8c1af7cca053ad0520b042d7ca1efa673d1990ec
Nam Pham <hainam.pham@innogames.com>
2019-07-02 18:57:24 +0200

    increase network size constants

--- Assets/Scripts/Model/Systems/SystemConstants.cs
+++ Assets/Scripts/Model/Systems/SystemConstants.cs
@@ -5,7 +5,7 @@ namespace Model.Systems
 		public const int NetworkNodeSize = 20;
 		public const int NetworkConnectionSize = NetworkNodeSize * NetworkNodeSize;
 
-		public const int MapConnectionSize = 100;		
-		public const int MapNodeSize = 100;
+		public const int MapConnectionSize = 500;		
+		public const int MapNodeSize = 500;
 	}
 }
